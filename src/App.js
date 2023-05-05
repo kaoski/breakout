1afb0a17d2a14808c7dfc9337f4fdf3ce684cad9
Kunal Acharya <kunal.acharya@oracle.com>
2023-05-05 22:23:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const SIZE = 20;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Paddle size={SIZE}/>
     </div>
   );
 }
+
+const Paddle = styled.div`
+  position: absolute;
+  background-color: red;
+  height: 20px;
+  width: 100px;
+  top: 20px;
+`
 
 export default App;
